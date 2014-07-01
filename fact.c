21f1161ed3fb507bf593dbf8630533fa878ffd7c
Nicolas Dechesne <ndec@linux.com>
2014-07-01 16:00:33 +0200

    fact.c continue until the user wants to stop
    and use 0 to detect the user wants to stop
    
    Signed-off-by: Nicolas Dechesne <ndec@linux.com>

--- fact.c
+++ fact.c
@@ -19,13 +19,21 @@ int main(void)
 {
 	unsigned long long res;
 	unsigned int n;
-	
+
 	/* Ask user to input a number */
-	printf ("Insert a number\n");
+	printf ("Insert a number, 0 to stop\n");
 	scanf ("%d", &n);
-	res = fact(n);
+
+	while(n != 0) {
+		res = fact(n);
+
+		/* Print result! */
+		printf ("fact(%d) = %llu\n", n, res);
+
+		/* Ask user to input a number */
+		printf ("Insert a number, 0 to stop\n");
+		scanf ("%d", &n);
+	}
 	
-	/* Print result! */
-	printf ("fact(%d) = %llu\n", n, res);
 	return 0;
 }
